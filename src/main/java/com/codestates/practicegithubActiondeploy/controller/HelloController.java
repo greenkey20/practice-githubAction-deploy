6c55cf232a629846eafffc627750dc618a161330
Eunyoung KANG <greenkey20@gmail.com>
2022-12-08 14:21:15 +0900

    fix: delete hello controller

--- src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
+++ src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
@@ -3,7 +3,8 @@ package com.codestates.practicegithubActiondeploy.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// 2022.12.8(목) 14h10
+// 2022.12.8(목) 14h10 접속 확인/성공 -> controller 없애고, 아까 404 페이지 안 보였던 것 다시 띄워보기
+/*
 @RestController
 public class HelloController {
 
@@ -13,3 +14,4 @@ public class HelloController {
     }
 
 }
+ */
